830872327ad67a30b02eed4a83ba0e5a4de11c40
Ashwin kamble <kamble.1985@gmail.com>
2020-07-10 02:31:39 +0530

    created classes for promotion

---
+++ BusinessLogicLayer/BaseClass/PromotionBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.BaseClass
+{
+    public abstract class PromotionBase
+    {
+        public PromotionBase(string productName)
+        {
+            this.Name = productName;
+        }
+        public string Name { get; set; }
+
+        public abstract void AddPromotion()
+        {
+
+        }
+    }
+}
---
+++ BusinessLogicLayer/BusinessObject/ProductDiscount.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLayer.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.BusinessObject
+{
+   public class ProductDiscount : PromotionBase
+    {
+        public string ProdName { get; set; }
+        public int ProdQuantity { get; set; }
+        public int ProdRate { get; set; }
+
+        public ProductDiscount(string productName, int productQuantity, int productRate) : base (productName)
+        {
+            this.ProdName = productName;
+            this.ProdQuantity = productQuantity;
+            this.ProdRate = productRate;
+        }
+
+        public override void AddPromotion()
+        {
+            
+        }
+    }
+}
